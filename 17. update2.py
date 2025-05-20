0fe833513716858a8222a52cfa8ed05b12874586
Basawoju Udaya Sri <141737684+UDAYASRIBASAWOJU@users.noreply.github.com>
2025-05-20 12:45:28 +0530

    updating data of entire column

---
+++ 17. update2.py
@@ -0,0 +1,12 @@
+data = {
+    "Name": ["Tiger", "Lion", "Elephant", "Cheetah", "Fox", "Giraffee", "Bear", "Panther", "Deer", "Rabbit"],
+    "Age": [20, 23, 34, 19, 24, 40, 38, 27, 23, 25],
+    "Salary": [38000, 45000, 23000, 56000, 34000, 29000, 70000, 46000, 52000, 26000],
+    "Experience": [3, 5, 2, 1, 5, 3, 2, 6, 8, 2]
+}
+
+df = pd.DataFrame(data)
+print(df)
+print(" ")
+df["Experience"] = df["Experience"] + 1
+print(df)
